f86432e480e37b4478991d430a4ebcff5eb03ed0
Karakaii <samu.dupret@hotmail.com>
2023-08-24 11:28:58 +0100

    positive_or_zero

--- functions_basic.R
+++ functions_basic.R
@@ -18,12 +18,12 @@ is_nan_data_frame <- function(x)
 # Functions
 #---------
 
-# Get number of unique instances
+## Get number of unique instances
 unique_n <- function(x) {
   return (length(unique(x)))
 }
 
-# Get standard error
+## Get standard error
 se <- function(x, na.rm = F) {
   if(na.rm) {x <- na.omit(x)}
   return(sd(x)/sqrt(length(x)))
@@ -32,4 +32,14 @@ se <- function(x, na.rm = F) {
 ## Function to get SE from mean and CI
 get_se_from_ci <- function(lower, upper){
   return((upper - lower)/3.92)
+}
+
+## Function for non-negative values
+# Returns the valule if positive, returns 0 if negative
+positive_or_zero <- function(x) {
+  if (x < 0) {
+    return(0)
+  } else {
+    return(x)
+  }
 }

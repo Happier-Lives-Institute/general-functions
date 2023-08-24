e1c7b23a72d2891881e8874d4e72fb3dd033a8d3
Karakaii <samu.dupret@hotmail.com>
2023-08-24 11:46:48 +0100

    ma_report_analysis

--- functions_meta_analysis.R
+++ functions_meta_analysis.R
@@ -144,3 +144,226 @@ ma_get_adjusted_se <- function(m) {
   
   return(adjusted_se)
 }
+
+## Report a fuller analysis of the RE or MLM model
+# Reports information about the moderation if relevant
+# Reports information about heterogeneity
+# Can flexibly do rma.mv and rma
+
+ma_report_analysis <- function(m) {
+  
+  #---
+  # general variables
+  #---
+  
+  # Is it a mlm format model (i.e., rma.mv)
+  is_mlm <- !is.null(m$random)
+  
+  # Number of effect sizes
+  k <- m$k.all
+  
+  # The weights based on sampling variance
+  wi <- 1/m$vi
+  
+  #---
+  # within-effect-sizes variance (nu)
+  #---
+  # The first step for most heterogeneity calculations is to get the typical
+  # within-effect-sizes variance.
+  
+  # This calculation is based on exploring the calculation in metafor::rma
+  # It looks complicated, because it takes into account possible moderators.
+  
+  # Create a diagonal matrix of the weights (which will weight the contributions
+  # of each effect size based on this precision)
+  W <- diag(wi, nrow = k, ncol = k)
+  
+  # Create a design matrix of the model (intercept plus any moderators)
+  # If there are no moderators you need to tell it to be an intercept only model
+  if(is.null(m$formula.mods)) (model_formula <- as.formula("~1")) else (model_formula <- m$formula.mods)
+  X <- model.matrix(model_formula, data = m$data)
+  
+  # Copying the rma function to inverse a product of matrices
+  .invcalc <- function (X, W, k) {
+    sWX <- sqrt(W) %*% X
+    res.qrs <- qr.solve(sWX, diag(k))
+    return(tcrossprod(res.qrs))
+  }
+  
+  # Get the inverse matrix of the variance weihts by the moderation
+  inverse_XWX <- .invcalc(X = X, W = W.FE, k = k)
+  
+  # Get a projection matrix of the previously calculated matrices
+  # (this often used to calculate residuals or other quantities that measure 
+  # the fit of a model)
+  P <- W - W %*% X %*% inverse_XWX %*% crossprod(X, W)
+  
+  # Get the number of predictors in the design matrix of this model
+  predictors_n <- NCOL(X)
+  
+  # Copying the rma function to return the trace of a matrix 
+  # (sum of diagonal elements)
+  .tr <- function (X) {return(sum(diag(X)))}
+  
+  # Calculate the within-effect-sizes variance (nu)
+  nu <- (k - predictors_n)/.tr(P)
+  
+  #---
+  # levels and tau
+  #---
+  # Prepare the levels structure that will be reported. 
+  # Get the tau2s for each level which is important for further calculations.
+  if(is_mlm) {
+    # Total number of levels set in the 
+    max_levels <- length(m$s.names)+1
+    
+    # Prepare the levels
+    model_levels <- data.frame(
+      level_number = seq(max_levels,1, -1),
+      level_formula = NA,
+      level_n = NA,
+      variance_symbol = NA, 
+      variance = NA,
+      nu = NA,
+      tau2 = NA
+    )
+    
+    # Add the descriptions of the levels
+    # Because the levels are written from the top to the bottom we need to go
+    # through these in the opposite direction.
+    for (level_i in seq(max_levels,1, -1)) {
+      if(level_i == max_levels) {
+        # First level is within-effect-size variance
+        model_levels$level_formula[level_i] <- "within-effect-size variance"
+        model_levels$level_n[level_i] <- k
+        model_levels$variance_symbol[level_i] <- "ε"
+        model_levels$variance[level_i] <- nu
+        model_levels$nu[level_i] <- nu
+        model_levels$tau2[level_i] <- NA
+      } else {
+        # Other levels are dependent on the formulation given in the model
+        # (-1 because the formulations don't have the within-effect-size variance)
+        model_levels$level_formula[level_i] <- m$s.names[level_i]
+        model_levels$level_n[level_i] <- m$s.nlevels[level_i]
+        model_levels$variance_symbol[level_i] <- paste0(
+          "ζ", model_levels$level_number[level_i]-1
+        )
+        model_levels$variance[level_i] <- m$sigma2[level_i]
+        model_levels$nu[level_i] <- NA
+        model_levels$tau2[level_i] <- m$sigma2[level_i]
+      }
+    }
+    
+  } else {
+    max_levels <- 2
+    # Only 2 levels if it is a random effects model
+    model_levels <- data.frame(
+      level_number = c(2, 1),
+      level_formula = c("between-effect-size-variance", "within-effect-size variance"),
+      level_n = k,
+      variance_symbol = c("ζ","ε"),
+      variance = c(m$tau2, nu),
+      nu = c(NA, nu),
+      tau2 = c(m$tau2, NA)
+    )
+  }
+  
+  #---
+  # I2
+  #---
+  # We need the sum of all the variances
+  sum_variance <- sum(model_levels$variance, na.rm = T)
+  # Get the I2 for each level
+  model_levels$I2 <- model_levels$tau2 / sum_variance
+  model_levels$`% of total variance` <- round_per(model_levels$I2)
+  model_levels$`% of total variance`[max_levels] <- round_per((1 - sum(model_levels$I2, na.rm = T)))
+  
+  #---
+  # ICC
+  #---
+  # % of the between-effect-size variance that is due to each level
+  model_levels$icc <- model_levels$tau2 / sum(model_levels$tau2, na.rm = T)
+  
+  #---
+  # H2
+  #---
+  # H2 is a ratio of all variance to the within-effect-size variance
+  model_levels$H2 <- sum_variance / nu
+  
+  #---
+  # Cochrane's Q
+  #---
+  
+  # Present all the information for Cochrane's Q test
+  Q <- list(
+    data = data.frame(Q = m$QE, df = k-predictors_n, p = m$QEp),
+    summary = paste0("Q(df = ", k-predictors_n, ") = ", round_c(m$QE), ", ", format_p_value(m$QEp))
+  )
+  
+  #---
+  # Prediction intervals
+  #---
+  # Get prediction intervals. It is unclear if this is exactly how it should
+  # be done for analyses with moderators.
+  
+  pi <- ma_get_adjusted_se(m)
+  
+  #---
+  # Moderators information
+  #---
+  
+  ## Calculate R2
+  
+  # Prepare the null model (model c, the version without moderators)
+  # If the model is not mlm, thereby an RE rma, we need to set the random level
+  # manually.
+  if(is_mlm) {
+    model_null_data <- m$data
+    model_null_random <- as.formula(m$random[[1]])
+  } else {
+    model_null_data <- m$data
+    model_null_data$es_id <- 1:nrow(model_null_data)
+    model_null_random <- as.formula("~1 | es_id")
+  }
+  model_null <- rma.mv(
+    yi = m$yi, V = m$vi, random = model_null_random, data = model_null_data,
+    method = m$method, test = m$test
+  )
+  
+  # Calculate R2 for the different models
+  model_levels$R2 <- NA
+  for (level_i in seq(max_levels,1, -1)) {
+    model_levels$R2[level_i] <- positive_or_zero(
+      (1 - (model_levels$tau2[level_i]/model_null$sigma2[level_i]))
+    )
+  }
+  
+  # Calculate the general R2
+  R2 <- positive_or_zero(
+    (1 - (sum(model_levels$tau2, na.rm = T)/sum(model_null$sigma2)))
+  )
+  
+  # Calculate overall how much the tau2 reduces from adding moderators
+  tau2_reduction_from_moderators <- sum(model_null$sigma2) - sum(model_levels$tau2, na.rm = T)
+  
+  # present all the information about the moderators
+  model <- list(
+    moderators_formula = model_formula,
+    parameters_n = predictors_n,
+    R2 = R2,
+    tau2 = sum(model_levels$tau2, na.rm = T),
+    tau2_reduction_from_moderators = tau2_reduction_from_moderators
+  )
+  
+  #---
+  # Prepare information for return
+  #---
+  results <- list(
+    model = model,
+    Q = Q,
+    model_levels = model_levels,
+    pi = pi
+  )
+  
+  return(results)
+}

00b2e9cac75f12bd646e164bbe3089f3adddccb6
Karakaii <samu.dupret@hotmail.com>
2023-09-13 21:05:52 +0100

    total effect function

--- functions_reporting.R
+++ functions_reporting.R
@@ -114,3 +114,123 @@ generate_linesep <- function(table, freq, insert = "\\addlinespace") {
   
   return(linesep_vec)
 }
+
+# Function to get the total effect with all the flexible methods we've used up to now
+get_total_effect <- function(
+    initial_centre, # mean
+    initial_spread, # se
+    initial_ll = -Inf, # lower limit
+    initial_ul = +Inf, # upper limit
+    trajectory_centre, # mean
+    trajectory_spread, # se
+    trajectory_ll = -Inf, # lower limit
+    trajectory_ul = +Inf, # upper limit
+    duration, # either individual_0, overall_0, or a value
+    WELLBY_conversion_rate = 1, # rate at which SDs are converted to WELLBYs
+    simulations, # number of simulations
+    illustration_fraction = 1, # fraction of simulations to take for graph
+    seed = NULL # seed for simulations
+) {
+  
+  # Run a potential seed
+  if(!is.null(seed)){set.seed(seed)}
+  
+  ## Prepare the first elements
+  total_effect_pe <- tibble(
+    initial = initial_centre,
+    trajectory = trajectory_centre
+  )
+  
+  total_effect_sim <- tibble(
+    initial = msm::rtnorm(
+      n = simulations,
+      mean = initial_centre,
+      sd = initial_spread,
+      lower = initial_ll,
+      upper = initial_ul
+    ),
+    trajectory = msm::rtnorm(
+      n = simulations,
+      mean = trajectory_centre,
+      sd = trajectory_spread,
+      lower = trajectory_ll,
+      upper = trajectory_ul
+    )
+  )
+  
+  ## Set the duration
+  # If each simulation gets its own individual duration
+  if(duration == "individual_0") {
+    total_effect_pe <- total_effect_pe %>% mutate(
+      duration = abs(initial/trajectory)
+    )
+    
+    total_effect_sim <- total_effect_sim %>% mutate(
+      duration = abs(initial/trajectory)
+    )
+  } else {
+    # If the duration is set to a certain value
+    # If it is set to when the point estimates reach 0
+    if(duration == "overall_0") {
+      duration_value = abs(initial_centre/trajectory_centre)
+    } else {
+      # otherwise set to value of the user
+      duration_value = duration
+    }
+    
+    total_effect_pe <- total_effect_pe %>% mutate(
+      duration = duration_value
+    )
+    total_effect_sim <- total_effect_sim %>% mutate(
+      duration = duration_value
+    )
+  }
+  
+  ## Calculate the total effect
+  total_effect_pe <- total_effect_pe %>% rowwise() %>% mutate(
+    total_effect = pracma::integral(function(t) {initial + trajectory * t}, 
+                                    0, duration),
+    total_effect_wellbys = total_effect*WELLBY_conversion_rate
+  ) %>% ungroup()
+  
+  total_effect_sim <- total_effect_sim %>% rowwise() %>% mutate(
+    total_effect = pracma::integral(function(t) {initial + trajectory * t}, 
+                                    0, duration),
+    total_effect_wellbys = total_effect*WELLBY_conversion_rate
+  ) %>% ungroup()
+  
+  ## Combine the two
+  total_effect_combined <- combine_PE_SIM(total_effect_pe, total_effect_sim)
+  
+  ## Make illustration graph
+  
+  # Make graph
+  total_effect_graph <- total_effect_sim %>% 
+    sample_frac(illustration_fraction) %>% 
+    mutate(
+      id = 1:(simulations*illustration_fraction),
+      end_effect = initial+trajectory*duration
+    ) %>% ggplot() +
+    geom_hline(yintercept = 0, linetype=2)+
+    geom_segment(aes(
+      x=0, xend=duration,
+      y=initial, yend=end_effect
+    ), alpha=0.25, color="#3167b4") +
+    geom_segment(aes(
+      x=0, xend=total_effect_pe$duration,
+      y=total_effect_pe$initial, 
+      yend=total_effect_pe$initial+
+        total_effect_pe$trajectory*total_effect_pe$duration
+    ), alpha=1, linewidth=1, color="orange") +
+    theme_cowplot() +
+    ylab("effect") + xlab("time")
+  
+  return(
+    list(
+      total_effect_pe       = total_effect_pe,
+      total_effect_sim      = total_effect_sim,
+      total_effect_combined = total_effect_combined,
+      total_effect_graph    = total_effect_graph
+    )
+  )
+}

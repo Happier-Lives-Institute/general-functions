5acd3b93254c7e6453bacc794af7dcd9ea99d462
Karakaii <samu.dupret@hotmail.com>
2024-04-10 16:24:50 +0100

    double_pooling_functions

--- functions_cohen_d.R
+++ functions_cohen_d.R
@@ -61,3 +61,31 @@ get_g <- function(d, n1, n2) {
 get_corrected_se_smd <- function(n1, n2) {
   return(sqrt(((n1 + n2)/(n1*n2))))
 }
+
+# Studies that have multiple arms compared between them (e.g., multiple 
+# treatment arms compared to one control arm) can lead to a unit-of-analysis
+# problem where some information is double counted.
+
+# The recommended solution from Cochrane is to combine the groups into a single
+# pair-wise comparison. This might lose some of the detail of the intervention but
+# is better than double counting.
+
+# https://training.cochrane.org/handbook/current/chapter-06#section-6-5-2-10
+# https://bookdown.org/MathiasHarrer/Doing_Meta_Analysis_in_R/es-calc.html#pool-groups
+# Prepare some functions 
+double_pool_n  <- function(n1, n2) {n1+n2}
+double_pool_m  <- function(n1, n2, m1, m2) {
+  # weighted average of the two - if both values are available
+  # otherwise, set to the one available value
+  if(is.na(m1) & is.na(m2)) {return(NA)}
+  else if(is.na(m1)){return(m2)}
+  else if(is.na(m2)){return(m1)}
+  else {return((n1*m1 + n2*m2)/(n1+n2))}
+}
+double_pool_sd <- function(n1, n2, m1, m2, sd1, sd2) {
+  sqrt(
+    ((n1-1)*(sd1^2) + (n2-1)*(sd2^2) + 
+       ((n1*n2)/(n1+n2))*(((m1^2)+(m2^2))-(2*m1*m2))) / (n1+n2-1)
+  )
+  
+}

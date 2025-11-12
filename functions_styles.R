e825e11047bd0622a1567f11d4943f65bfb50bb5
Karakaii <samu.dupret@hotmail.com>
2025-11-12 16:16:18 +0000

    double save png and svg

--- functions_styles.R
+++ functions_styles.R
@@ -6,3 +6,23 @@ theme_hli_wbg <- function() {
       plot.background = element_rect(fill = "white", colour = NA)
     )
 }
+
+# Function to save a plot as both PNG and SVG
+hli_double_save <- function(filename_no_end, plot, width, height, dpi, units = "in"){
+  ggsave(
+    filename = paste0(filename_no_end, ".png"),
+    plot = plot,
+    width = width,
+    height = height,
+    dpi = dpi,
+    units = units
+  )
+  ggsave(
+    filename = paste0(filename_no_end, ".svg"),
+    plot = plot,
+    width = width,
+    height = height,
+    dpi = dpi,
+    units = units
+  )
+}

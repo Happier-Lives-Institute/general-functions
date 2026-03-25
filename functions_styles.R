a7075210938ae0fbb95eab3d74c733ae3a62980b
Karakaii <samu.dupret@hotmail.com>
2026-03-25 15:56:59 +0000

    update double save

--- functions_styles.R
+++ functions_styles.R
@@ -8,21 +8,38 @@ theme_hli_wbg <- function() {
 }
 
 # Function to save a plot as both PNG and SVG
-hli_double_save <- function(filename_no_end, plot, width, height, dpi, units = "in"){
+hli_double_save <- function(filename_no_end, plot, width, height, dpi, units = "in") {
+
+  # --- PNG ---
   ggsave(
     filename = paste0(filename_no_end, ".png"),
-    plot = plot,
-    width = width,
-    height = height,
-    dpi = dpi,
-    units = units
+    plot     = plot,
+    width    = width,
+    height   = height,
+    dpi      = dpi,
+    units    = units
   )
+
+  # --- SVG ---
+  svg_path <- paste0(filename_no_end, ".svg")
+
   ggsave(
-    filename = paste0(filename_no_end, ".svg"),
-    plot = plot,
-    width = width,
-    height = height,
-    dpi = dpi,
-    units = units
+    filename = svg_path,
+    plot     = plot,
   )
+
+  svg_string <- readChar(svg_path, file.info(svg_path)$size)
+
+  # Remove CDATA wrapper (breaks inline SVG in WordPress)
+  svg_string <- gsub("<![CDATA[", "", svg_string, fixed = TRUE)
+  svg_string <- gsub("]]>",       "", svg_string, fixed = TRUE)
+
+  # Remove XML declaration (breaks inline SVG embedding)
+  svg_string <- gsub("^<\\?xml[^\\?]*\\?>\\s*", "", svg_string, perl = TRUE)
+
+  # Make responsive: width=100%, drop fixed height (viewBox preserves aspect ratio)
+  svg_string <- gsub("(<svg[^>]*) width='[^']*'",  "\\1 width='100%'", svg_string, perl = TRUE)
+  svg_string <- gsub("(<svg[^>]*) height='[^']*'", "\\1",              svg_string, perl = TRUE)
+
+  writeChar(svg_string, svg_path, eos = NULL)
 }

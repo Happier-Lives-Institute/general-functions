3a36c65e45c4eb225832ffc3342ce44a4f73d4b8
Karakaii <samu.dupret@hotmail.com>
2026-08-01 17:29:05 +0100

    add failsafe

--- functions_meta_analysis.R
+++ functions_meta_analysis.R
@@ -34,7 +34,7 @@ ma_extract_weights <- function(m) {
 # and Beyond" by Judd et al. (2017)
 #  - https://github.com/StatQuest/logistic_regression_demo/blob/master/logistic_regression_demo.R
 ma_model_comparison <- function (m1, m2){
-  
+
   # Get which model is the reduced (C) or full (A)
   if(m1$parms <= m2$parms) {
     mC <- m1
@@ -43,18 +43,18 @@ ma_model_comparison <- function (m1, m2){
     mC <- m2
     mA <- m1
   }
-  
+
   # Calculate the logLik difference
   df <- mA$parms - mC$parms
   logLik_C <- -2*logLik(mC)[1]
   logLik_A <- -2*logLik(mA)[1]
   logLik_diff <- abs(logLik_C - logLik_A) # abs to deal with rare case of positive logLik
   PRD <- abs(logLik_diff / logLik_C)
-  
+
   # Check whether the logLik difference is statistically significant
   chi_crit <- qchisq(1-.05, df = df)
   p_value <- pchisq(logLik_diff, df=df, lower.tail=F)
-  
+
   # Present the models and their values
   models_table <- data.frame(
     n_parameters_C = mC$parms,
@@ -68,7 +68,7 @@ ma_model_comparison <- function (m1, m2){
     names_to = c(".value", "model"),
     names_pattern = "(.*)_(.*)"
   ); models_table <- as.data.frame(models_table)
-  
+
   # Present the logLik comparison
   output_table <- data.frame(
     PRD=PRD,
@@ -85,7 +85,7 @@ ma_model_comparison <- function (m1, m2){
   ) %>% mutate(
     bigger_model_better_both = bigger_model_better_logLik & bigger_model_better_AIC
   )
-  
+
   # Return a list with information calculated here
   # and information about the model specifications
   return(
@@ -105,13 +105,13 @@ ma_model_comparison <- function (m1, m2){
 ma_get_adjusted_se <- function(m) {
   # Is it a mlm format model (i.e., rma.mv)
   is_mlm <- !is.null(m$random)
-  
+
   # Get the total number of parameter
   params <- m$p
-  
+
   # Get the total between-effect-sizes variance
   if(is_mlm) (tau2 <- sum(m$sigma2)) else (tau2 <- m$tau2)
-  
+
   # Prepare the data
   adjusted_se <- NULL
   # Loop through and populate the data
@@ -119,12 +119,12 @@ ma_get_adjusted_se <- function(m) {
 
     # Adjust ddf based on whether it is mlm or not
     if(is_mlm) (ddf <- m$ddf[[i]]) else (ddf <- m$ddf[1])
-    
+
     # Get the prediction interval
     t_crit = qt(0.05/2, df = ddf, lower.tail = FALSE)
     pi_lb = m$b[i] - t_crit * sqrt(tau2 + m$se[i]^2)
     pi_ub = m$b[i] + t_crit * sqrt(tau2 + m$se[i]^2)
-    
+
     new_adjusted_se <- data.frame(
       param = i,
       se    = m$se[i],
@@ -137,11 +137,11 @@ ma_get_adjusted_se <- function(m) {
       pi_ub,
       adjusted_se_pi = (pi_ub-pi_lb)/3.92
     )
-    
+
     # Adding the lines
     adjusted_se <- rbind(adjusted_se, new_adjusted_se)
   }
-  
+
   return(adjusted_se)
 }
 
@@ -153,42 +153,42 @@ ma_get_adjusted_se <- function(m) {
   return(tcrossprod(res.qrs))
 }
 
-## Copying the rma function to return the trace of a matrix 
+## Copying the rma function to return the trace of a matrix
 # (sum of diagonal elements)
 .tr <- function (X) {return(sum(diag(X)))}
 
-# A big complication of the model is that I want to get the nu. This is 
-# difficult because there is a lot of stuff happening. If I can simplify it 
+# A big complication of the model is that I want to get the nu. This is
+# difficult because there is a lot of stuff happening. If I can simplify it
 # into one function that words, I can reuse it more easily.
 
 get_nu_rma.mv <- function(m) {
-  
+
   #---
   # general variables
   #---
-  
+
   # Is it a mlm format model (i.e., rma.mv)
   is_mlm <- !is.null(m$random)
-  
+
   # Number of effect sizes
   k <- m$k.eff
-  
+
   # The weights based on sampling variance
   wi <- 1/m$vi
-  
+
   #---
   # within-effect-sizes variance (nu)
   #---
   # The first step for most heterogeneity calculations is to get the typical
   # within-effect-sizes variance.
-  
+
   # This calculation is based on exploring the calculation in metafor::rma
   # It looks complicated, because it takes into account possible moderators.
-  
+
   # Create a diagonal matrix of the weights (which will weight the contributions
   # of each effect size based on this precision)
   W <- diag(wi, nrow = k, ncol = k)
-  
+
   # Create a design matrix of the model (intercept plus any moderators)
   # If there are no moderators you need to tell it to be an intercept only model
   # First, we need to prepare the data, filtering NAs for any of the moderators
@@ -200,24 +200,24 @@ get_nu_rma.mv <- function(m) {
     m_data <- m$data %>%
       filter(!if_any(all_of(all.vars(model_formula)), is.na))
   }
-  
+
   # Then we can create the model matrix
   X <- model.matrix(model_formula, data = m_data)
-  
+
   # Get the inverse matrix of the variance weihts by the moderation
   inverse_XWX <- .invcalc(X = X, W = W, k = k)
-  
+
   # Get a projection matrix of the previously calculated matrices
-  # (this often used to calculate residuals or other quantities that measure 
+  # (this often used to calculate residuals or other quantities that measure
   # the fit of a model)
   P <- W - W %*% X %*% inverse_XWX %*% crossprod(X, W)
-  
+
   # Get the number of predictors in the design matrix of this model
   predictors_n <- NCOL(X)
-  
+
   # Calculate the within-effect-sizes variance (nu)
   nu <- (k - predictors_n)/.tr(P)
-  
+
   return(
     list(
       nu=nu, predictors_n=predictors_n
@@ -231,27 +231,27 @@ get_nu_rma.mv <- function(m) {
 # Can flexibly do rma.mv and rma
 
 ma_report_analysis <- function(m) {
-  
+
   #---
   # general variables
   #---
-  
+
   # Is it a mlm format model (i.e., rma.mv)
   is_mlm <- !is.null(m$random)
-  
+
   # Number of effect sizes
   k <- m$k.eff
-  
+
   # The weights based on sampling variance
   wi <- 1/m$vi
-  
+
   #---
   # within-effect-sizes variance (nu)
   #---
   # The first step for most heterogeneity calculations is to get the typical
-  # within-effect-sizes variance. 
+  # within-effect-sizes variance.
   # This uses a separate function so to be repeated.
-  
+
   # Create a design matrix of the model (intercept plus any moderators)
   # If there are no moderators you need to tell it to be an intercept only model
   # First, we need to prepare the data, filtering NAs for any of the moderators
@@ -263,33 +263,33 @@ ma_report_analysis <- function(m) {
     m_data <- m$data %>%
       filter(!if_any(all_of(all.vars(model_formula)), is.na))
   }
-  
+
   # Get the number of predictors in the design matrix of this model
   predictors_n <- get_nu_rma.mv(m)$predictors_n
-  
+
   # Calculate the within-effect-sizes variance (nu)
   nu <- get_nu_rma.mv(m)$nu
-  
+
   #---
   # levels and tau
   #---
-  # Prepare the levels structure that will be reported. 
+  # Prepare the levels structure that will be reported.
   # Get the tau2s for each level which is important for further calculations.
   if(is_mlm) {
-    # Total number of levels set in the 
+    # Total number of levels set in the
     max_levels <- length(m$s.names)+1
-    
+
     # Prepare the levels
     model_levels <- data.frame(
       level_number = seq(max_levels,1, -1),
       level_formula = NA,
       level_n = NA,
-      variance_symbol = NA, 
+      variance_symbol = NA,
       variance = NA,
       nu = NA,
       tau2 = NA
     )
-    
+
     # Add the descriptions of the levels
     # Because the levels are written from the top to the bottom we need to go
     # through these in the opposite direction.
@@ -315,7 +315,7 @@ ma_report_analysis <- function(m) {
         model_levels$tau2[level_i] <- m$sigma2[level_i]
       }
     }
-    
+
   } else {
     max_levels <- 2
     # Only 2 levels if it is a random effects model
@@ -329,12 +329,12 @@ ma_report_analysis <- function(m) {
       tau2 = c(m$tau2, NA)
     )
   }
-  
+
   #---
   # Tau
   #---
   model_levels$tau <- sqrt(model_levels$tau2)
-  
+
   #---
   # I2
   #---
@@ -344,62 +344,74 @@ ma_report_analysis <- function(m) {
   model_levels$I2 <- model_levels$tau2 / sum_variance
   model_levels$`% of total variance` <- round_per(model_levels$I2)
   model_levels$`% of total variance`[max_levels] <- round_per((1 - sum(model_levels$I2, na.rm = T)))
-  
+
   #---
   # ICC
   #---
   # % of the between-effect-size variance that is due to each level
   model_levels$icc <- model_levels$tau2 / sum(model_levels$tau2, na.rm = T)
-  
+
   #---
   # H2
   #---
   # H2 is a ratio of all variance to the within-effect-size variance
   model_levels$H2 <- sum_variance / nu
-  
+
   #---
   # Cochrane's Q
   #---
-  
+
   # Present all the information for Cochrane's Q test
   Q <- list(
     data = data.frame(Q = m$QE, df = k-predictors_n, p = m$QEp),
     summary = paste0("Q(df = ", k-predictors_n, ") = ", round_c(m$QE), ", ", format_p_value(m$QEp))
   )
-  
+
   #---
   # Prediction intervals
   #---
   # Get prediction intervals. It is unclear if this is exactly how it should
   # be done for analyses with moderators.
-  
+
   pi <- ma_get_adjusted_se(m)
-  
+
   #---
   # Moderators information
   #---
-  
+
   ## Calculate R2
-  
+
   # Only if this is not an FE model
   if(m$method != "FE") {
-    
+
     # Prepare the null model (model c, the version without moderators)
     # If the model is not mlm, thereby an RE rma, we need to set the random level
     # manually.
     if(is_mlm) {
       model_null_data <- m_data
-      model_null_random <- as.formula(m$random[[1]])
+      model_null_random <- m$random
+      model_null_struct <- m$struct
     } else {
       model_null_data <- m_data
       model_null_data$es_id <- 1:nrow(model_null_data)
       model_null_random <- as.formula("~1 | es_id")
+      model_null_struct <- "CS"
     }
-    model_null <- rma.mv(
-      yi = m$yi, V = m$vi, random = model_null_random, data = model_null_data,
-      method = m$method, test = m$test
+    model_null <- tryCatch(
+      rma.mv(
+        yi = m$yi, V = m$vi, random = model_null_random, struct = model_null_struct,
+        data = model_null_data, method = m$method, test = m$test
+      ),
+      error = function(e) {
+        message("Null model fit failed (", conditionMessage(e), "); retrying with raised iterations.")
+        rma.mv(
+          yi = m$yi, V = m$vi, random = model_null_random, struct = model_null_struct,
+          data = model_null_data, method = m$method, test = m$test,
+          control = list(iter.max = 10000, eval.max = 10000, rel.tol = 1e-8)
+        )
+      }
     )
-    
+
     # Calculate R2 for the different models
     model_levels$R2 <- NA
     for (level_i in seq(max_levels,1, -1)) {
@@ -407,28 +419,28 @@ ma_report_analysis <- function(m) {
         (1 - (model_levels$tau2[level_i]/model_null$sigma2[level_i]))
       )
     }
-    
+
     # Calculate the general R2
     R2 <- positive_or_zero(
       (1 - (sum(model_levels$tau2, na.rm = T)/sum(model_null$sigma2)))
     )
-    
+
     # Calculate overall how much the tau2 reduces from adding moderators
     tau2_reduction_from_moderators <- sum(model_null$sigma2) - sum(model_levels$tau2, na.rm = T)
-    
+
     # Calculate the I2 from the model without moderators
     model_null_nu <- get_nu_rma.mv(model_null)$nu
     model_null_sum_variance <- sum(model_null$sigma2, na.rm = T) + model_null_nu
     model_null_I2 <- sum(model_null$sigma2) / model_null_sum_variance
     I2_reduction_from_moderators <- model_null_I2 - sum(model_levels$I2, na.rm = T)
-    
+
   } else {
     # Make NA for FE model
     R2 <- NA
     tau2_reduction_from_moderators <- NA
     I2_reduction_from_moderators <- NA
   }
-  
+
   # present all the information about the moderators
   model <- list(
     moderators_formula = model_formula,
@@ -440,7 +452,7 @@ ma_report_analysis <- function(m) {
     tau2_reduction_from_moderators = tau2_reduction_from_moderators,
     I2_reduction_from_moderators = I2_reduction_from_moderators
   )
-  
+
   #---
   # Prepare information for return
   #---
@@ -450,7 +462,7 @@ ma_report_analysis <- function(m) {
     model_levels = model_levels,
     pi = pi
   )
-  
+
   return(results)
 }
 
@@ -458,42 +470,45 @@ ma_report_analysis <- function(m) {
 my_funnel_rma.mv <- function(
     m, # the model we are graphing for
     add_contour = T, # Whether there is a contour plot
-    se_variable = "function_se", # the variable to use if we want to set a different se, 
+    se_variable = "function_se", # the variable to use if we want to set a different se,
     # such as corrected SEs
-    point_fill = "" # Variable by which we group the effect sizes in color
+    point_fill = "", # Variable by which we group the effect sizes in color
+    xlab = "g"
 ) {
-  
+
   # Extract data and estimates from rma.mv model
   dat <- m$data
   dat <- as.data.frame(dat)
   dat$yi <- as.numeric(m$yi)
   dat$function_se <- sqrt(m$vi)
   dat$function_se <- dat[, se_variable]
-  
+  dat$g_se_precision <- 1/dat$function_se
+  dat$z <- dat$yi / dat$function_se
+
   # Prepare general elements of the graph
   pooled_effect <- m$b[1]
   max_se <- max(dat$function_se)
-  
+
   # Calculate expected (or pseudo) confidence interval for the maximum SE
   expected_ci <- calculate_ci_se(pooled_effect, max_se)
-  
+
   # Generate the funnel plot
   my_funnel_plot <- dat %>% ggplot(aes(x = yi, y = function_se)) +
     # Reverse the standard errors
     scale_y_reverse() +
-    
+
     # Prepare the theme
     theme_cowplot() +
     labs(x = "Effect size",
          y = "Standard Error")
-  
+
   # Conditionally add the contour plot
   if (add_contour) {
     contour_data <- data.frame(
       x = c(
         c(0, -1.64 * max_se, +1.64 * max_se),
-        c(0, -1.64 * max_se, -1.96 * max_se), 
-        c(0, 1.64 * max_se, 1.96 * max_se), 
+        c(0, -1.64 * max_se, -1.96 * max_se),
+        c(0, 1.64 * max_se, 1.96 * max_se),
         c(0, -2.58 * max_se, -1.96 * max_se),
         c(0, 2.58 * max_se, 1.96 * max_se),
         c(0, -2.58 * max_se, -Inf),
@@ -510,25 +525,25 @@ my_funnel_rma.mv <- function(
         rep("p < .001", 3)
       )
     )
-    
+
     my_funnel_plot <- my_funnel_plot +
       geom_polygon(data = contour_data, aes(x = x, y = y, fill = fill)) +
       scale_fill_manual(
-        values = c("n.s." = "gray75", "p < .10" = "gray85", 
+        values = c("n.s." = "gray75", "p < .10" = "gray85",
                    "p < .05" = "gray95", "p < .001" = "white"),
         breaks = c("p < .001", "p < .05", "p < .10", "n.s."),
         name = "Significance Level",
         drop = F
       )
   }
-  
+
   # Continue with other elements of the funnel plot
   my_funnel_plot <- my_funnel_plot +
     # Create the line for the predicted effect size
     geom_segment(aes(
       x = pooled_effect, xend = pooled_effect, y = Inf, yend = 0
     ), linetype = 3) +
-    
+
     # Create the funnel
     geom_segment(aes(
       x = expected_ci[1], xend = pooled_effect, y = Inf, yend = 0
@@ -536,11 +551,11 @@ my_funnel_rma.mv <- function(
     geom_segment(aes(
       x = pooled_effect, xend = expected_ci[2], y = 0, yend = Inf
     ), linetype=3)
-  
+
   # Dependent on whether a point fill was given, add the effect sizes
   if (point_fill != "") {
     my_funnel_plot <- my_funnel_plot +
-      geom_point(alpha = 0.5, 
+      geom_point(alpha = 0.5, shape = 16,
                  aes(fill = get(point_fill), color = get(point_fill))) +
       # need to modify the legend accordingly
       guides(
@@ -550,16 +565,27 @@ my_funnel_rma.mv <- function(
       theme(legend.position = "bottom", legend.box = "vertical")
   } else {
     my_funnel_plot <- my_funnel_plot +
-      geom_point(alpha = 0.5, shape = 21, color = "black", fill = "blue")
+      geom_point(
+        alpha = 0.5,
+        shape = 16,
+        # color = "blue"
+      )
   }
 
   # Make a white background that is easier to visualise
-  my_funnel_plot <- my_funnel_plot + theme(
-    plot.background = element_rect(fill = "white"),
-    panel.background = element_rect(fill = "white")
-  )
-  
+  my_funnel_plot <- my_funnel_plot +
+    our_theme() + xlab(xlab)
+
   # return the funnel
   return(my_funnel_plot)
-  
+
+}
+
+# Function to find the se threshold for powered studies
+power_se_threshold <- function(alpha = 0.05, power = 0.80) {
+  z_alpha <- qnorm(1 - alpha / 2)
+  z_beta  <- qnorm(power)
+  sum_z   <- z_alpha + z_beta
+
+  return(sum_z)
 }
